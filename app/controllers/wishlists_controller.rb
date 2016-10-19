dcb3fb878f4430779661a2dd6acc0989e8726565
bearnie <bearniezeborg@gmail.com>
2016-10-19 15:35:45 +0200

    Wishlist Create from beer show, need to add quantity to submit

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,4 +1,4 @@
-class WishlistController < ApplicationController
+class WishlistsController < ApplicationController
   before_filter :find_model
 
   def new
@@ -7,13 +7,14 @@ class WishlistController < ApplicationController
 
   def create
     @wishlist = Wishlist.new({ user_id: current_user.id, beer_id: params[:beer_id] })
+    @wishlist.save
+    redirect_to dashboard_path
   end
 
 
   private
+
   def find_model
     @model = Wishlist.find(params[:id]) if params[:id]
   end
-
-
 end
