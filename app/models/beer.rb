30a2de598b3e7712fa96c402eac39bbc17989492
marc jarsaillon <marc.jarsaillon@gmail.com>
2016-10-20 12:03:56 +0200

    set defautl value to stock

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,7 +2,8 @@ class Beer < ApplicationRecord
 
   validates :name, :category, presence: true
   belongs_to :user
-  has_many :wishlists
+  has_many :wishlists, dependent: :destroy
+
 
   mount_uploader :photo, PhotoUploader
 
